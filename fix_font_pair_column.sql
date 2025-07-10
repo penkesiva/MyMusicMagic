4314ba61fe617fc2e44cafeab3fab2c0feeeff4e
Siva <penkesiva@gmail.com>
2025-07-10 00:18:31 -0700

    clean up sql file
    Signed-off-by: Siva <penkesiva@gmail.com>

--- fix_font_pair_column.sql
+++
@@ -1,7 +0,0 @@
--- Add font_pair column to user_portfolios table
--- This column stores the selected font combination
-
-ALTER TABLE user_portfolios ADD COLUMN IF NOT EXISTS font_pair TEXT DEFAULT 'montserrat-merriweather';
-
--- Success message
-SELECT 'Font pair column added successfully!' as status; 
